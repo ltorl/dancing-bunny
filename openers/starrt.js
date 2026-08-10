24b7842e65aa169f7ed55566561f71243bd05d06
ltorl <126298377+ltorl@users.noreply.github.com>
2026-08-10 09:47:21 -1000

    push

--- openers/starrt.js
+++ openers/starrt.js
@@ -1,19 +1,19 @@
-fetch("https://cdn.jsdelivr.net/gh/ltorl/dancing-bunny@main/public/v2.html")
-    .then(response => response.text())
-    .then(text => { 
-        const newWin = window.open("about:blank", "_blank");
-        if (newWin) { 
-            newWin.document.open(); 
-            newWin.document.write(text); 
-            newWin.document.close(); 
-
-            setTimeout(() => {
-                const existingIcons = doc.querySelectorAll('link[rel~=\'icon\']');
-                existingIcons.forEach(oldIcon => {
-                    const newIcon = oldIcon.cloneNode(true);
-                    oldIcon.remove();
-                    doc.head.appendChild(newIcon);
-                });
-            }, 51);
-        } 
-    });
+const newWin = window.open('about:blank', '_blank');
+if (newWin) {
+	fetch('https://cdn.jsdelivr.net/gh/ltorl/dancing-bunny@main/public/v2.html')
+		.then(response => response.text())
+		.then(text => {
+			const doc = newWin.document;
+			doc.open();
+			doc.write(text);
+			doc.close();
+			setTimeout(() => {
+				const existingIcons = doc.querySelectorAll('link[rel=\'icon\']');
+				existingIcons.forEach(oldIcon => {
+					const newIcon = oldIcon.cloneNode(true);
+					oldIcon.remove();
+					doc.head.appendChild(newIcon);
+				});
+			}, 51);
+		})
+}
